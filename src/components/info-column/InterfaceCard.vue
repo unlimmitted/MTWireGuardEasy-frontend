9e2aa0746c4728a3abc252b2189b88fbd68f8b66
unlimmitted <kosnmasss@gmail.com>
2024-09-29 02:53:07 +0300

    added submit btn to delete interface action

--- src/components/info-column/InterfaceCard.vue
+++ src/components/info-column/InterfaceCard.vue
@@ -18,7 +18,7 @@
 					flat
 					round
 					icon="delete"
-					@click="this.deleteInterface"
+					@click="this.confirmModal = true"
 				>
 					<q-tooltip>
 						{{ this.deleteTooltipText }}
@@ -52,6 +52,28 @@
 			</div>
 		</div>
 	</q-card>
+	<q-dialog v-model="this.confirmModal">
+		<q-card>
+			<q-card-section>
+				Confirm interface deletion: {{ this.interface.name }}
+			</q-card-section>
+			<q-card-actions style="justify-content: end">
+				<q-btn
+					outline
+					v-close-popup
+				>
+					Close
+				</q-btn>
+				<q-btn
+					style="width: 30%"
+					color="primary"
+					@click="this.deleteInterface"
+				>
+					Confirm
+				</q-btn>
+			</q-card-actions>
+		</q-card>
+	</q-dialog>
 </template>
 <script>
 import {useStore} from "../../store.js";
@@ -60,6 +82,9 @@ import axios from "axios";
 export default {
 	name: "InterfaceCard",
 	props: ["interface"],
+	data: () => ({
+		confirmModal: false
+	}),
 	methods: {
 		deleteInterface() {
 			axios.post("/api/v1/delete-external-interface", this.interface)
